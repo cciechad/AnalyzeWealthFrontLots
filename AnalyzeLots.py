1692537e1d3e9194a8abfde74d5ac718e84f64e8
Chad A Simmons <chad.simmons@member.fsf.org>
2024-03-06 14:56:42 -0500

    Add ,.2f formatting to print statements to improve readability

--- AnalyzeLots.py
+++ AnalyzeLots.py
@@ -25,9 +25,9 @@ def main() -> None:
                 args.file, header=1, names=['symbol', 'display_name', 'date', 'cost', 'quantity', 'value', 'gain'])
         data['date'] = pandas.to_datetime(data['date'])
         if not args.nosummary:
-            print(f"Total gain/loss ${round(data['gain'].sum(), 2)}")
-            print(f"Short term gain/loss ${round(data.loc[data['date'] >= one_year_prior, 'gain'].sum(), 2)}")
-            print(f"Long term gain/loss ${round(data.loc[data['date'] < one_year_prior, 'gain'].sum(), 2)}")
+            print(f"Total gain/loss ${round(data['gain'].sum(), 2):,.2f}")
+            print(f"Short term gain/loss ${round(data.loc[data['date'] >= one_year_prior, 'gain'].sum(), 2):,.2f}")
+            print(f"Long term gain/loss ${round(data.loc[data['date'] < one_year_prior, 'gain'].sum(), 2):,.2f}")
         if args.symbol | args.nosummary:
             symbols: list[str] = data['symbol'].str.split(',\s*').explode().unique().tolist()
             symbols_gain: list[float] = []
@@ -43,13 +43,13 @@ def main() -> None:
                         data.loc[(data['symbol'] == iter_symbol) & (data['date'] >= one_year_prior), 'gain'].sum(0), 2))
             symbols_dict: dict = {symbols[i]: symbols_gain[i] for i in range(len(symbols))}
             print('Total gain/loss per symbol')
-            print("\n".join(f"{k}\t${v}" for k, v in symbols_dict.items()))
+            print("\n".join(f"{k}\t${v:,.2f}" for k, v in symbols_dict.items()))
             symbols_short_dict: dict = {symbols[i]: symbols_gain_short[i] for i in range(len(symbols))}
             print('Short term per symbol')
-            print("\n".join(f"{k}\t${v}" for k, v in symbols_short_dict.items()))
+            print("\n".join(f"{k}\t${v:,.2f}" for k, v in symbols_short_dict.items()))
             symbols_long_dict: dict = {symbols[i]: symbols_gain_long[i] for i in range(len(symbols))}
             print('Long term per symbol')
-            print("\n".join(f"{k}\t${v}" for k, v in symbols_long_dict.items()))
+            print("\n".join(f"{k}\t${v:,.2f}" for k, v in symbols_long_dict.items()))
 
 
 if __name__ == '__main__':
