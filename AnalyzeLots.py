14cca4f6d6af22d9ab036afde8131dc0520d2142
Chad A Simmons <chad.simmons@member.fsf.org>
2024-03-07 14:59:18 -0500

    extract out symbols_net_print function and add error message if args.file is not a file

--- AnalyzeLots.py
+++ AnalyzeLots.py
@@ -6,23 +6,6 @@ from pathlib import Path
 import pandas
 
 
-def parse_args() -> argparse.Namespace:
-    parser: argparse.ArgumentParser = argparse.ArgumentParser(
-        description="Analyze Wealthfront cost-basis data. Displays net short/long term gains/losses and total "
-                    "short/long term losses by default.", epilog="File required")
-    parser.add_argument('-s', '--symbol',
-                        help='Display net gain/loss by symbol and net short/long-term gain/loss per symbol',
-                        action='store_true')
-    parser.add_argument('-n', '--no-summary', help='No summary', action='store_true')
-    parser.add_argument('-f', '--file', help='File to process', type=lambda p: Path(p).absolute(), required=True)
-    return parser.parse_args()
-
-
-def format_dollar(amount: float) -> str:
-    formatted_absolute_amount: str = '${:,.2f}'.format(abs(amount))
-    return f'-{formatted_absolute_amount}' if round(amount, 2) < 0 else formatted_absolute_amount
-
-
 def main() -> None:
     args: argparse.Namespace = parse_args()
     if args.file.is_file():
@@ -51,15 +34,38 @@ def main() -> None:
                 symbols_net_long.append(data.loc[is_iter_symbol & is_long, 'gain'].sum(0))
                 symbols_net_short.append(data.loc[is_iter_symbol & is_short, 'gain'].sum(0))
             symbols_range: range = range(len(symbols))
-            symbols_dict: dict = {symbols[i]: symbols_net[i] for i in symbols_range}
             print('Net gain/loss per symbol')
-            print("\n".join(f"{k}\t{format_dollar(v)}" for k, v in symbols_dict.items()))
-            symbols_short_dict: dict = {symbols[i]: symbols_net_short[i] for i in symbols_range}
+            symbols_net_print(symbols, symbols_net, symbols_range)
             print('Net short term per symbol')
-            print("\n".join(f"{k}\t{format_dollar(v)}" for k, v in symbols_short_dict.items()))
-            symbols_long_dict: dict = {symbols[i]: symbols_net_long[i] for i in symbols_range}
+            symbols_net_print(symbols, symbols_net_short, symbols_range)
             print('Net long term per symbol')
-            print("\n".join(f"{k}\t{format_dollar(v)}" for k, v in symbols_long_dict.items()))
+            symbols_net_print(symbols, symbols_net_long, symbols_range)
+    else:
+        print(f'{args.file} is not a readable file.')
+    return
+
+
+def parse_args() -> argparse.Namespace:
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="Analyze Wealthfront cost-basis data. Displays net short/long term gains/losses and total "
+                    "short/long term losses by default.", epilog="File required")
+    parser.add_argument('-s', '--symbol',
+                        help='Display net gain/loss by symbol and net short/long-term gain/loss per symbol',
+                        action='store_true')
+    parser.add_argument('-n', '--no-summary', help='No summary', action='store_true')
+    parser.add_argument('-f', '--file', help='File to process', type=lambda p: Path(p).absolute(), required=True)
+    return parser.parse_args()
+
+
+def symbols_net_print(symbols: list[str], net: list[float], symbols_range: range) -> None:
+    symbols_dict: dict = {symbols[i]: net[i] for i in symbols_range}
+    print("\n".join(f"{k}\t{format_dollar(v)}" for k, v in symbols_dict.items()))
+    return
+
+
+def format_dollar(amount: float) -> str:
+    formatted_absolute_amount: str = '${:,.2f}'.format(abs(amount))
+    return f'-{formatted_absolute_amount}' if round(amount, 2) < 0 else formatted_absolute_amount
 
 
 if __name__ == '__main__':
